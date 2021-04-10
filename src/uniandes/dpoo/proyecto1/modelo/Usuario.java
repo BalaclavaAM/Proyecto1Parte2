f0443f1376e813541cc08f10515b36695ca9ac17
Juan Manuel Camacho Pérez <jm.camachop1@uniandes.edu.co>
2021-04-10 18:53:04 -0500

    Añadida Clase User
    Bv

---
+++ src/uniandes/dpoo/proyecto1/modelo/Usuario.java
@@ -0,0 +1,26 @@
+package uniandes.dpoo.proyecto1.modelo;
+
+public class Usuario {
+	// ************************************************************************
+	// Atributos
+	// ************************************************************************
+	private String nombredeusuario;
+	private String contrasenha;
+	
+	
+	public String getContrasenha() {
+		return contrasenha;
+	}
+	
+	public void setContrasenha(String contrasenha) {
+		this.contrasenha = contrasenha;
+	}
+	
+	public String getNombredeusuario() {
+		return nombredeusuario;
+	}
+	
+	public void setNombredeusuario(String nombredeusuario) {
+		this.nombredeusuario = nombredeusuario;
+	}
+}
