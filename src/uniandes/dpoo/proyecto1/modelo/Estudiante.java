e2c6ac3a495f2a719289fd812f33ce7fe6b8d8d4
Juan Manuel Camacho Pérez <jm.camachop1@uniandes.edu.co>
2021-04-10 21:12:06 -0500

    momazos dalgo

--- src/uniandes/dpoo/proyecto1/modelo/Estudiante.java
+++ src/uniandes/dpoo/proyecto1/modelo/Estudiante.java
@@ -1,5 +1,27 @@
 package uniandes.dpoo.proyecto1.modelo;
 
-public class Estudiante {
-
+public class Estudiante extends Usuario {
+	private String name;
+	private String codigo;
+	private PlandeEstudios plan;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public PlandeEstudios getPlan() {
+		return plan;
+	}
+	public void setPlan(PlandeEstudios plan) {
+		this.plan = plan;
+	}
+	
 }
---
+++ src/uniandes/dpoo/proyecto1/modelo/PlandeEstudios.java
@@ -0,0 +1,13 @@
+package uniandes.dpoo.proyecto1.modelo;
+
+public class PlandeEstudios {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
